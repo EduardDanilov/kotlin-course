37f810ee5fdc9a25d8e203bc4857f59dcc8b69dd
Eduard <ed.danilov94@gmail.com>
2025-09-14 17:54:59 +0300

    add homework for lesson 3

---
+++ src/main/kotlin/lessons/lesson3/homework/hw.kt
@@ -0,0 +1,57 @@
+package lessons.lesson3.homework
+
+import javax.swing.plaf.multi.MultiPanelUI
+
+val v1: Short = 42
+val v2: Long = 98765432123456789L
+val v3: Float = 23.45f
+val v4: Double = 0.123456789
+val v5: String = "Kotlin & Java"
+//val v6 = FALSE (правильно false)
+val v6: Boolean = false
+val v7: Char = 'c'
+val v8: Short = 500
+val v9: Long = 4294967296L
+val v10: Float = 18.0f
+val v11: Double = -0.001
+val v12: String = "OpenAI"
+val v13: String = "true"
+val v14: List<Int> = listOf(3, 14)
+val v15: Char = '9'
+val v16: Short = 2048
+val v17: Long = 10000000000L
+val v18: Set<String> = setOf("OpenAI", "Quantum Computing")
+val v19: Float = 5.75f
+//val v20 = `1.414` (обратные кавычки нельзя тут использовать)
+val v20: Double = 1.414
+val v21: String = "Artificial Intelligence"
+//val v22: Array<Any> = arrayOf('x', "A") (если тут есть ошибка, то будет Array<Any> и 'A')
+val v22: Array<Any> = arrayOf('x', "A")
+val v23: String = "Android Studio"
+val v24: Char = '@'
+val v25: Short = 1024
+val v26: Long = 1234567890123L
+val v27: Float = 10.01f
+val v28: Double = -273.15
+val v29: String = "SpaceX"
+//val v30 = FALSE (false должно быть с маленькой буквы)
+val v30: Boolean = false
+val v31: Double = 0.007
+//val v32 = “🤯” (ошибка в кавычках)
+val v32: String = "🤯"
+//val v33: = mapOf("true" to 2, "false" to 34) (тут либо Map<String, Int>, либо Map<Boolean, Int>. Второй вариант больше похож на правду)
+val v33: Map<Boolean, Int> = mapOf(true to 2, false to 34)
+//val v34 = ‘65535’ (ошибка в кавычках)
+val v34: Int = 65535
+val v35: Long = 72057594037927935L
+val v36: Float = 2.71828f
+val v37: Double = 101.0101
+val v38: String = "Quantum Computing"
+//val v39 = mapOf(2 to "true", 34 to "false") (значение либо строка, либо булево. Булево будет правильным)
+val v39: Map<Int, Boolean> = mapOf(2 to true, 34 to false)
+val v40: Char = 'x'
+val v41: Short = 314
+val v42: Long = 123456789123456789L
+val v43: Float = 6.626f
+//val v44 = FALSE (правильно false)
+val v44: Boolean = false
